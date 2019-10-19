fef69df5b989d63ead9b83de2a31f46d9fa5556a
paulkastel <pkastelik@poczta.fm>
2019-10-19 11:28:08 +0200

    Created main landing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,19 @@
+import 'package:cash_calc/views/main_page_view.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 void main() {
-  print('started!');
+  runApp(CashCalcApp());
+}
+
+class CashCalcApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      title: 'Cash Calculator',
+      theme: ThemeData(primarySwatch: Colors.lightGreen, fontFamily: 'Barlow'),
+      home: MainPageView()
+    );
+  }
 }
