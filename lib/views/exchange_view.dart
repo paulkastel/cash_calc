05f22f4ff3fb79cf95aec486db6c0b186e425ffd
paulkastel <pkastelik@poczta.fm>
2019-10-19 22:47:39 +0200

    added animation on changing button

--- lib/views/exchange_view.dart
+++ lib/views/exchange_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cash_calc/views/components/textfield_cash.dart';
 import 'package:flutter/material.dart';
 
@@ -6,35 +8,87 @@ class ExchangeMoneyView extends StatefulWidget {
   _ExchangeMoneyViewState createState() => _ExchangeMoneyViewState();
 }
 
-class _ExchangeMoneyViewState extends State<ExchangeMoneyView> {
-  TextEditingController txtEditCtrlr = TextEditingController();
+class _ExchangeMoneyViewState extends State<ExchangeMoneyView>
+    with TickerProviderStateMixin {
+  AnimationController _animationCtrlr;
+  bool _flipDirection = true;
+  final TextEditingController _txtEditCtrlr = TextEditingController();
+
+  @override
+  void dispose() {
+    _animationCtrlr.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _animationCtrlr = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  void animateIcon() {
+    if (_flipDirection) {
+      setState(() => _flipDirection = false);
+      _animationCtrlr.forward();
+    } else {
+      _animationCtrlr.reverse();
+      setState(() => _flipDirection = true);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Text('how many dolars are euro',
-            style: Theme.of(context).textTheme.headline),
-        TextFieldCash(txtEditCtrlr, () {
-          print(txtEditCtrlr.text);
-        }),
-        Text('Currancy rate of usd: 4.3',
-            style: Theme.of(context).textTheme.headline),
-        FlatButton(
-          padding: const EdgeInsets.all(20),
-          color: Theme.of(context).primaryColorDark,
-          splashColor: Theme.of(context).accentColor,
-          child: Icon(
-            Icons.import_export,
-            color: Theme.of(context).iconTheme.color,
-            size: 60,
-          ),
-          shape: CircleBorder(),
-          onPressed: () {
-            print('fuck');
-          },
+        Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: Text('Check foregin currency value',
+              style: Theme.of(context).textTheme.headline),
         ),
-        const Text('your output is'),
+        TextFieldCash(_txtEditCtrlr, () {
+          print(_txtEditCtrlr.text);
+        }),
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Text('Currency rate of usd: 4.3',
+              style: Theme.of(context).textTheme.headline),
+        ),
+        AnimatedBuilder(
+            animation: _animationCtrlr,
+            builder: (BuildContext context, Widget child) {
+              return FlatButton(
+                padding: const EdgeInsets.all(15),
+                color: Theme.of(context).primaryColorDark,
+                splashColor: Theme.of(context).accentColor,
+                shape: CircleBorder(),
+                onPressed: () {
+                  animateIcon();
+                },
+                child: Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()
+                    ..rotateX(2 *
+                        pi *
+                        Tween(begin: 0.0, end: 0.5)
+                            .animate(CurvedAnimation(
+                                parent: _animationCtrlr,
+                                curve: Interval(0.0, 0.7, curve: Curves.ease)))
+                            .value),
+                  child: Icon(
+                    Icons.import_export,
+                    color: Theme.of(context).iconTheme.color,
+                    size: 40,
+                  ),
+                ),
+              );
+            }),
+        const Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Text('Value:'),
+        ),
       ],
     );
   }
