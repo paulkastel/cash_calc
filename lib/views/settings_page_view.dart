446b2ebced5a90fb2d7a68c70e22f79cd16ccbb3
paulkastel <pkastelik@poczta.fm>
2019-10-20 09:56:58 +0200

    added setting ui for base currency

--- lib/views/settings_page_view.dart
+++ lib/views/settings_page_view.dart
@@ -1,7 +1,15 @@
+import 'package:cash_calc/models/currency_model.dart';
 import 'package:flutter/material.dart';
 
 @immutable
-class SettingsPageView extends StatelessWidget {
+class SettingsPageView extends StatefulWidget {
+  @override
+  _SettingsPageViewState createState() => _SettingsPageViewState();
+}
+
+class _SettingsPageViewState extends State<SettingsPageView> {
+  Currency _selectedCurrency = currencies[0];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +24,38 @@ class SettingsPageView extends StatelessWidget {
           'Settings',
         ),
       ),
-      body: Text(
-        'You are in settings',
-        style: Theme.of(context).textTheme.body1,
+      body: Column(
+        children: <Widget>[
+          Text(
+            'You are in settings',
+            style: Theme.of(context).textTheme.body1,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                const Text('Select base currency: '),
+                DropdownButton<Currency>(
+                  underline: const SizedBox(),
+                  value: _selectedCurrency,
+                  onChanged: (Currency selected) {
+                    setState(() {
+                      _selectedCurrency = selected;
+                    });
+                  },
+                  items: currencies.map((Currency currency) {
+                    return DropdownMenuItem<Currency>(
+                      value: currency,
+                      child: Text('${currency.flag} ${currency.isoCode}',
+                          style: Theme.of(context).textTheme.body1),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
