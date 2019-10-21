4b38ec3dd5ac1109a5c8e69a2521200ceef1ff16
paulkastel <pkastelik@poczta.fm>
2019-10-21 23:36:29 +0200

    Wrapped view with singlescrollchildview

--- lib/views/exchange_view.dart
+++ lib/views/exchange_view.dart
@@ -42,55 +42,58 @@ class _ExchangeMoneyViewState extends State<ExchangeMoneyView>
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(top: 20.0),
-          child: Text(AppTexts.of(context).convertforeginCurrencyVal,
-              style: Theme.of(context).textTheme.headline),
-        ),
-        TextFieldCash(_txtEditCtrlr, () {
-          print(_txtEditCtrlr.text);
-        }),
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Text(AppTexts.of(context).currencyRateOf+' usd: 4.3',
-              style: Theme.of(context).textTheme.headline),
-        ),
-        AnimatedBuilder(
-            animation: _animationCtrlr,
-            builder: (BuildContext context, Widget child) {
-              return FlatButton(
-                padding: const EdgeInsets.all(15),
-                color: Theme.of(context).primaryColorDark,
-                splashColor: Theme.of(context).accentColor,
-                shape: CircleBorder(),
-                onPressed: () {
-                  animateIcon();
-                },
-                child: Transform(
-                  alignment: Alignment.center,
-                  transform: Matrix4.identity()
-                    ..rotateX(2 *
-                        pi *
-                        Tween(begin: 0.0, end: 0.5)
-                            .animate(CurvedAnimation(
-                                parent: _animationCtrlr,
-                                curve: Interval(0.0, 0.7, curve: Curves.ease)))
-                            .value),
-                  child: Icon(
-                    Icons.import_export,
-                    color: Theme.of(context).iconTheme.color,
-                    size: 40,
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Text(AppTexts.of(context).convertforeginCurrencyVal,
+                style: Theme.of(context).textTheme.headline),
+          ),
+          TextFieldCash(_txtEditCtrlr, () {
+            print(_txtEditCtrlr.text);
+          }),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(AppTexts.of(context).currencyRateOf + ' usd: 4.3',
+                style: Theme.of(context).textTheme.headline),
+          ),
+          AnimatedBuilder(
+              animation: _animationCtrlr,
+              builder: (BuildContext context, Widget child) {
+                return FlatButton(
+                  padding: const EdgeInsets.all(15),
+                  color: Theme.of(context).primaryColorDark,
+                  splashColor: Theme.of(context).accentColor,
+                  shape: CircleBorder(),
+                  onPressed: () {
+                    animateIcon();
+                  },
+                  child: Transform(
+                    alignment: Alignment.center,
+                    transform: Matrix4.identity()
+                      ..rotateX(2 *
+                          pi *
+                          Tween(begin: 0.0, end: 0.5)
+                              .animate(CurvedAnimation(
+                                  parent: _animationCtrlr,
+                                  curve:
+                                      Interval(0.0, 0.7, curve: Curves.ease)))
+                              .value),
+                    child: Icon(
+                      Icons.import_export,
+                      color: Theme.of(context).iconTheme.color,
+                      size: 40,
+                    ),
                   ),
-                ),
-              );
-            }),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Text(AppTexts.of(context).valueText+':'),
-        ),
-      ],
+                );
+              }),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(AppTexts.of(context).valueText + ':'),
+          ),
+        ],
+      ),
     );
   }
 }
