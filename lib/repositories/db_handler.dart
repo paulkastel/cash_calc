e621692666524e243afbe0243d0b773a791c8e81
paulkastel <pkastelik@poczta.fm>
2019-10-22 00:01:38 +0200

    Added plugins for databse

--- lib/repositories/db_handler.dart
+++ lib/repositories/db_handler.dart
@@ -2,15 +2,27 @@ library api.app_database;
 
 import 'package:cash_calc/models/currency_model.dart';
 import 'package:cash_calc/models/user_model.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 DbHandler dbHandler;
+
 // TODO: add sqlie
 // TODO: add tests
 class DbHandler {
   DbHandler() {
     appUser = User(currencies[4]);
   }
+
   User appUser;
+
+  /// TODO: this databse is needed to be finished, think about some good db structure
+  Future<void> _initializeAppDataBase() async {
+    final Future<Database> database = openDatabase(
+      join(await getDatabasesPath(), 'cash_calc.db'),
+      version: 1,
+    );
+  }
 
   void updateBaseCurrency(Currency newMoney) {
     appUser.baseCurrency = newMoney;
